07ac7ae38dea39e0a71fbb2253d3bc35126517c8
Christian Lazo <christian-lazo-u@hotmail.com>
2023-10-18 16:58:12 -0500

    add properties and importsql!

---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO books (id, title, author, gender, year_of_publication ) VALUES ('1', 'test01', '1', '1', '2023-08-12');
+INSERT INTO books (id, title, author, gender, year_of_publication ) VALUES ('2', 'test01', '1', '1', '2023-08-12');
+INSERT INTO books (id, title, author, gender, year_of_publication ) VALUES ('3', 'test01', '1', '1', '2023-08-12');
+INSERT INTO books (id, title, author, gender, year_of_publication ) VALUES ('4', 'test01', '1', '1', '2023-08-12');
+INSERT INTO books (id, title, author, gender, year_of_publication ) VALUES ('5', 'test01', '1', '1', '2023-08-12');
+INSERT INTO books (id, title, author, gender, year_of_publication ) VALUES ('6', 'test01', '1', '1', '2023-08-12');
+INSERT INTO books (id, title, author, gender, year_of_publication ) VALUES ('7', 'test01', '1', '1', '2023-08-12');
